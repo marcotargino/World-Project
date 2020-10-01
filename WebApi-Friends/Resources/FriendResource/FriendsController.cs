449cbf0a3d898e6b3290b9aac8b1d4324e84c66c
Marco Targino <marco.targino@al.infnet.edu.br>
2020-09-30 23:12:25 -0300

    erros resolvidos

--- WebApi-Friends/Resources/FriendResource/FriendsController.cs
+++ WebApi-Friends/Resources/FriendResource/FriendsController.cs
@@ -1,47 +1,92 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using System.Security.Cryptography;
 
 namespace WebApi_Friends.Resources.FriendResource
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/friends")]
     public class FriendsController : ControllerBase
     {
-        // GET: api/<FriendsController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            var list = new List<FriendResponse>();
+
+            return Ok(list);
         }
 
         // GET api/<FriendsController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public ActionResult Get(string id)
         {
-            return "value";
+            var response = SearchFriendById(id);
+
+            if (response == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(response);
+            }
+        }
+
+        private object SearchFriendById(string id)
+        {
+            throw new NotImplementedException();
         }
 
         // POST api/<FriendsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post([FromBody] FriendRequest friendRequest)
         {
+            var response = CreateFriend(friendRequest);
+
+            return CreatedAtAction(nameof(Get), new { response.Id }, response);
+        }
+
+        private FriendResponse CreateFriend(FriendRequest friendRequest)
+        {
+            throw new NotImplementedException();
         }
 
         // PUT api/<FriendsController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult Put(int id, [FromBody] string value)
         {
         }
 
         // DELETE api/<FriendsController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
         }
+    }
+
+    public class FriendRequest
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public String ProfilePicture { get; set; }
+        public String Email { get; set; }
+        public String PhoneNumber { get; set; }
+        public String Country { get; set; }
+        public String State { get; set; }
+    }
+
+    public class FriendResponse
+    {
+        public Guid Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public String ProfilePicture { get; set; }
+        public String Email { get; set; }
+        public String PhoneNumber { get; set; }
+        public String Country { get; set; }
+        public String State { get; set; }
     }
 }
