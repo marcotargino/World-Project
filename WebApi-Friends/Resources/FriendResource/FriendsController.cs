51ed89d881c273768f67ad5b4eec7caf18f17e75
Marco Targino <marco.targino@al.infnet.edu.br>
2020-10-01 17:03:52 -0300

    api returns

--- WebApi-Friends/Resources/FriendResource/FriendsController.cs
+++ WebApi-Friends/Resources/FriendResource/FriendsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography;
 
 namespace WebApi_Friends.Resources.FriendResource
@@ -14,7 +15,7 @@ namespace WebApi_Friends.Resources.FriendResource
         {
             var list = new List<FriendResponse>();
 
-            return Ok(list);
+            return Ok(list); //200
         }
 
         // GET api/<FriendsController>/5
@@ -29,7 +30,7 @@ namespace WebApi_Friends.Resources.FriendResource
             }
             else
             {
-                return Ok(response);
+                return Ok(response); //200
             }
         }
 
@@ -42,9 +43,16 @@ namespace WebApi_Friends.Resources.FriendResource
         [HttpPost]
         public ActionResult Post([FromBody] FriendRequest friendRequest)
         {
+            var errors = friendRequest.Errors();
+
+            if (errors.Any())
+            {
+                return UnprocessableEntity(errors);
+            }
+
             var response = CreateFriend(friendRequest);
 
-            return CreatedAtAction(nameof(Get), new { response.Id }, response);
+            return CreatedAtAction(nameof(Get), new { response.Id }, response); //201
         }
 
         private FriendResponse CreateFriend(FriendRequest friendRequest)
@@ -54,14 +62,46 @@ namespace WebApi_Friends.Resources.FriendResource
 
         // PUT api/<FriendsController>/5
         [HttpPut("{id}")]
-        public ActionResult Put(int id, [FromBody] string value)
+        public ActionResult Put(string id, [FromBody] FriendRequest request)
         {
+            var response = SearchFriendById(id);
+
+            if (response == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                ModifyFriend(request);
+                return NoContent(); //204
+            }
+        }
+
+        private void ModifyFriend(FriendRequest request)
+        {
+            throw new NotImplementedException();
         }
 
         // DELETE api/<FriendsController>/5
         [HttpDelete("{id}")]
-        public ActionResult Delete(int id)
+        public ActionResult Delete(string id)
         {
+            var response = SearchFriendById(id);
+
+            if (response == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                RemoveFriend(id);
+                return NoContent(); //204
+            }
+        }
+
+        private void RemoveFriend(string id)
+        {
+            throw new NotImplementedException();
         }
     }
 
@@ -75,6 +115,11 @@ namespace WebApi_Friends.Resources.FriendResource
         public String PhoneNumber { get; set; }
         public String Country { get; set; }
         public String State { get; set; }
+
+        internal List<string> Errors()
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class FriendResponse
